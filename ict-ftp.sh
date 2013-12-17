84ef0cc5fa8802f01f3f957a3d02401ec9c10bd4
Peter Gamache <peter+github@duonet.net>
2013-12-17 15:56:48 -0600

    Delete ict-ftp.sh

--- ict-ftp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-workdir=`mktemp -d`
-cd ${workdir:?}
-email="david.niemeyer@wellsfargo.com, nicholas.gillis@wellsfargo.com"
-ncftpget -Z -V -r 3 -d - -u wfmg -p 'q#!DQDVC' ftp1.ictgroup.com ${workdir:?} /outbox/WFMG_WT2_INTERVAL.xls > logfile 2>&1
-mpack -a -s "ICT UPDATE" -d logfile WFMG_WT2_INTERVAL.xls ${email:?}
-cd - >/dev/null
-rm -rf ${workdir:?}
